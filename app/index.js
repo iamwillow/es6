04d00845ccd6229c8c407b593beafc1bc6984611
wshelley <wshelley@indeed.com>
2018-07-17 21:45:55 +0900

    es6 local vs global scope

---
+++ app/index.js
@@ -0,0 +1,29 @@
+// LOCAL vs GLOBAL SCOPE
+
+let limit = 100; // global scope
+
+{
+  let limit = 10; // local scope
+  console.log(`backstage limit ${limit}`);
+}
+
+console.log(`total limit ${limit}`);
+
+// ------ //
+// ------ //
+// ------ //
+
+// another example of LOCAL vs GLOBAL SCOPE
+
+function hello() {
+  let message = "hello";
+  console.log(message);
+}
+
+function greeting() {
+  let message = "how are you?";
+  console.log(message);
+}
+
+hello();
+greeting();
