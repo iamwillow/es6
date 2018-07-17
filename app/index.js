fa50d87cfac206eea3063dde54c9758f248a105b
wshelley <wshelley@indeed.com>
2018-07-17 22:17:44 +0900

    es6 local vs global scope

--- app/index.js
+++ app/index.js
@@ -1,38 +1,7 @@
-// LOCAL vs GLOBAL SCOPE
+import scope from './lessons/01_scope.js';
 
-let limit = 100; // global scope
+scope();
 
-{
-  let limit = 10; // local scope
-  console.log(`backstage limit ${limit}`);
-}
-
-console.log(`total limit ${limit}`);
-
-
-// ------ //
-// ------ //
-// ------ //
-
-// another example of LOCAL vs GLOBAL SCOPE
-
-function hello() {
-  let message = "hello";
-  console.log(message);
-}
-
-function greeting() {
-  let message = "how are you?";
-  console.log(message);
-}
-
-hello();
-greeting();
-
-
-// ------ //
-// ------ //
-// ------ //
 
 // SPREAD OPERATOR for arrays and objects 
 
