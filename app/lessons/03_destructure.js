a7dd3b4da5f8b5c2d8daa8e5ac95a553de1f359e
wshelley <wshelley@indeed.com>
2018-07-17 22:29:52 +0900

    es6 destructuring

---
+++ app/lessons/03_destructure.js
@@ -0,0 +1,61 @@
+const destructure = () => {
+  console.log('>>> DESTRUCTURING:');
+
+
+// destructuring ARRAYS
+{
+  let a = [4, 5, 6];
+  let first = a[0];
+  let second = a[1];
+  console.log(`${first}, ${second}`);
+}
+
+{
+  let a = [4, 5, 6];
+  let [first, second] = a;
+  console.log(`${first}, ${second}`);
+}
+
+// destructuring OBJECTS
+{
+  let lion = { name: 'mufasa', kids: 1};
+  let name = lion.name;
+  let kids = lion.kids;
+  console.log(name, kids);
+}
+
+{
+  let lion = { name: 'mufasa', kids: 1};
+  let {name, kids} = lion;
+  console.log(name, kids);
+}
+
+{
+  let lion = { name: 'mufasa', kids: {name: 'simba'}};
+  let {name, kids} = lion;
+  console.log(name, kids);
+}
+
+
+// An error that may arise
+// Problem: {name, kids} is understood as a coding block
+{
+  // let lion = { name: 'mufasa', kids: 1};
+  // let name, kids;
+  // {name, kids} = lion;
+  // console.log(name, kids);
+} 
+
+// Solution: add ()
+{
+  let lion = { name: 'mufasa', kids: 1};
+  let name, kids;
+  ({name, kids} = lion);
+  console.log(name, kids);
+}
+
+
+
+}
+
+export default destructure;
