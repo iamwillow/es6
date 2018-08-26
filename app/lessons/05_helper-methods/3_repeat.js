7aa6607c2dc4659fec9936ee52a70574aa97b60d
wshelley <wshelley@indeed.com>
2018-08-26 23:20:50 +0900

    moved comment to index

--- app/lessons/05_helper-methods/3_repeat.js
+++ app/lessons/05_helper-methods/3_repeat.js
@@ -3,8 +3,6 @@ const repeat = () => {
   console.log('>>> REPEATING:');
 
   const koolAid = `${"o".repeat(30)}"hh yeah!"`;
-  // remember `` is called a: template string
-
   console.log(koolAid);
 
 }
